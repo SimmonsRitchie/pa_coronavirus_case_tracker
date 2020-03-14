bbc1c8d4ea8043c25e3953a176948fd31b2c5467
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-14 19:58:12 -0400

    Fix: render children properly

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,12 @@
 import React from "react";
 
-class Body extends React.Component {
-  render() {
-    return (
-      <div className="body__container">
-        {...this.props.children}
-      </div>
-    );
-  }
+const Body = (props) => {
+  return ( 
+    <div className="body__container">
+    {props.children}
+  </div>
+   );
 }
-
+ 
 export default Body;
+
