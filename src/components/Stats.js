87c060ce8506a314b1ee44d4a56227d4e50549b3
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-14 14:15:00 -0400

    Create Stats component

---
+++ src/components/Stats.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Stats = () => {
+  return ( 
+    <div>
+    <div>Confirmed infections: </div>
+    <div>Deaths: </div>
+    </div> );
+}
+ 
+export default Stats;
