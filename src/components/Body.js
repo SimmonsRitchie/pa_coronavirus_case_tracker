0962f80513b93c4b56701c3bd6fd267842f0c9ab
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-14 14:15:20 -0400

    Convert Body into a container component

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,7 +4,7 @@ class Body extends React.Component {
   render() {
     return (
       <div className="body__container">
-        Insert map/chart/interactive thing here.
+        {...this.props.children}
       </div>
     );
   }
