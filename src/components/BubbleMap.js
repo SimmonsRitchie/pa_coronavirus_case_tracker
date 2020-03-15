014cb8f0ee5764208144fd6bb755b453daccc9aa
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-15 13:22:29 -0400

    Rename Map component as BubbleMap, implement react-simple-maps

---
+++ src/components/BubbleMap.js
@@ -0,0 +1,73 @@
+import React, { useContext } from "react";
+import {
+  ComposableMap,
+  ZoomableGroup,
+  Geographies,
+  Geography
+} from "react-simple-maps";
+import { DataContext } from "../context/DataContext";
+
+const BubbleMap = () => {
+  const PA_CENTER = [-77.641, 40.989];
+  const { data } = useContext(DataContext);
+  const countyMap = data.countyMap;
+  console.log(countyMap);
+  // console.log(kittens.countyMap)
+
+  return (
+    <div className="map__container">
+      <ComposableMap
+      data-tip={""}
+      projection={"geoMercator"}
+        projectionConfig={{
+          scale: 7500,
+        }}
+        width={773}
+        height={449}
+        style={{
+          width: "100%",
+          height: "auto"
+        }}
+      >
+        <ZoomableGroup 
+        center={PA_CENTER} 
+        zoom={1}>
+          <Geographies geography={countyMap}>
+            {({ geographies }) =>
+              geographies.map(geo => {
+                return (
+                  <Geography
+                    key={geo.rsmKey}
+                    geography={geo}
+                    fill="green"
+                    style={{
+                      default: {
+                        stroke: "white",
+                        strokeWidth: 0.75,
+                        outline: "none"
+                      },
+                      hover: {
+                        opacity: "0.5",
+                        stroke: "white",
+                        strokeWidth: 2,
+                        outline: "pink"
+                      },
+                      pressed: {
+                      opacity: "0.5",
+                      stroke: "white",
+                      strokeWidth: 0.75,
+                      outline: "none"
+                      }
+                    }
+                    }
+                  />
+                );
+              })
+            }
+          </Geographies>
+        </ZoomableGroup>
+      </ComposableMap>
+    </div>)
+};
+
+export default BubbleMap;
