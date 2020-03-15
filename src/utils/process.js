a98d3eefa03b40b2bf16c631c0d87169443699de
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-15 15:35:52 -0400

    Convert funcs into class

--- src/utils/process.js
+++ src/utils/process.js
@@ -1,29 +1,34 @@
 import { feature } from "topojson-client";
 import {geoCentroid} from "d3-geo"
 
+class ProcessTopo {
+  constructor(topoJson) {
+    this.topoJson = topoJson
+  }
 
-export const convertTopo2Geo = (topojson, objectsName ) => {
-  /* Returns geojson featires from topojson object. Often needed for working with D3.
-  * @params:
-  * topojson (object): topojson file to transform
-  * objectsName (str): Name of object property where feature is located. eg. "gz_2010_us_040_00_500k"
-  */
-  return feature(topojson, {
-    type: "GeometryCollection",
-    geometries: topojson.objects[objectsName]["geometries"]
-  });
-}
+  convertTopo2Geo(objectsName) {
+    /* Returns geojson featires from topojson object. Often needed for working with D3.
+    * @params:
+    * objectsName (str): Name of object property where feature is located. eg. "gz_2010_us_040_00_500k"
+    */
+    this.geoJson = (this.topojson, {
+      type: "GeometryCollection",
+      geometries: this.topojson.objects[objectsName]["geometries"]
+    });
+    return this
 
-export const getCentroids = (geoJson) => {
-// Returns an array of centroids from geojson object
-  console.log(geoJson)
-  const arrCentroids = geoJson.features.map(feature => {
-    console.log(feature)
-    const centroid = geoCentroid(feature)
-    console.log(centroid)
-    return centroid
-  })
-  console.log(arrCentroids)
-  return arrCentroids
+  }
 
+  getCentroids() {
+    // Returns an array of centroids from geojson object
+      console.log(this.geoJson)
+      this.arrCentroids = this.geoJson.features.map(feature => {
+        console.log(feature)
+        const centroid = geoCentroid(feature)
+        console.log(centroid)
+        return centroid
+      })
+      console.log(arrCentroids)
+      return this.arrCentroids
+    }
 }
