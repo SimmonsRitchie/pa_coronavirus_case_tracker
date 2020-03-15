b0244e16481c19ca7708096484fe95c6e4ecf024
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-15 18:47:38 -0400

    Refactor: Use DataContext, parsing funcs

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useContext } from "react";
+import {getTotalFromSheet} from "../utils/parse"
+import { DataContext } from "../context/DataContext";
 
-const Stats = ({cases, deaths}) => {
-  return ( 
+
+const Stats = () => {
+  const { data } = useContext(DataContext);
+  const cases = getTotalFromSheet(data.paCases)
+  const deaths = getTotalFromSheet(data.paDeaths)
+
+  return (
     <div>
-    <div>Cases</div>
-    <div>{cases ? cases : "..."}</div>
-    <div>Deaths</div>
-    <div>{deaths ? deaths : "..."}</div>
-    </div> );
-}
- 
+      <div>Cases</div>
+      <div>{cases ? cases : "..."}</div>
+      <div>Deaths</div>
+      <div>{deaths ? deaths : "..."}</div>
+    </div>
+  );
+};
+
 export default Stats;
