a612c3d65b00b820bcdf3d36fb7a39f119bf25e3
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-16 22:01:53 -0400

    Create ScaleRadius

---
+++ src/utils/ScaleRadius.js
@@ -0,0 +1,20 @@
+import { max} from "d3-array"
+import {scaleSqrt} from "d3-scale"
+
+class ScaleRadius {
+  constructor(dataArr, maxRange=40) {
+    this.dataArr = dataArr
+    this.maxVal = max(dataArr) 
+    this.maxRange = maxRange
+    console.log("Max val",this.maxVal)
+  }
+
+  radius(num) {
+    const processingFunc = scaleSqrt()
+      .domain([0, this.maxVal])
+      .range([0, this.maxRange]);
+    return processingFunc(num)
+  }
+}
+
+export default ScaleRadius
