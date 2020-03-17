31bf2c2da45e2c6a2246fedf89928f61a90864ca
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-17 17:01:13 -0400

    Add react-vis + implement

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,7 +1,24 @@
 import React from 'react';
+import {FlexibleXYPlot, LineSeries, HorizontalGridLines, XAxis, YAxis} from "react-vis";
 
 const LineChart = () => {
-  return (  );
+  return (
+  <div className="line-chart__container">
+    <FlexibleXYPlot
+      // height={300}
+      // width={700}
+      colorType="category"
+      colorDomain={[0, 1, 2]}
+      colorRange={myPalette}
+    >
+      <LineSeries data={series1} color={0} />
+      <LineSeries data={series2} color={1} />
+      <LineSeries data={series3} color={2} />
+    <HorizontalGridLines />
+      <XAxis />
+      <YAxis />
+    </FlexibleXYPlot>
+  </div> );
 }
  
 export default LineChart;
