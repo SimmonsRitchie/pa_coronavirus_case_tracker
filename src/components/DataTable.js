0204cdee55234876c377b5f0318ad8e6f2cc8866
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-18 17:44:11 -0400

    Add table headers

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,16 +1,24 @@
-import React, { useContext } from 'react';
+import React, { useContext } from "react";
 import { DataContext } from "~/context/DataContext";
 
 const DataTable = () => {
   const { data } = useContext(DataContext);
-  const countyTotals = data.countyTotals
-  console.log(countyTotals)
+  const countyTotals = data.countyTotals;
+  console.log(countyTotals);
 
-  return ( 
-    <div>
-      Data table!
+  return (
+    <div className="data-table__container">
+      <table className="table">
+        <thead>
+          <tr>
+            <th>County </th>
+            <th>Cases</th>
+            <th>Deaths</th>
+          </tr>
+        </thead>
+      </table>
     </div>
-   );
-}
- 
+  );
+};
+
 export default DataTable;
