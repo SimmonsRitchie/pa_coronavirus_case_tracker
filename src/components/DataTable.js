6e669ec859ef2e8fd81fcb9d9b728621e9a50c33
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-18 17:32:02 -0400

    Pull in county totals

--- src/components/DataTable.js
+++ src/components/DataTable.js
@@ -1,6 +1,11 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { DataContext } from "~/context/DataContext";
 
 const DataTable = () => {
+  const { data } = useContext(DataContext);
+  const countyTotals = data.countyTotals
+  console.log(countyTotals)
+
   return ( 
     <div>
       Data table!
