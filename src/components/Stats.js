0bc621b4b57b778bd7ab39f32d9d4ba9d2af30c7
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-19 11:32:04 -0400

    Restructure containers, refactor Stats css

--- src/components/Stats.js
+++ src/components/Stats.js
@@ -1,22 +1,33 @@
 import React, { useContext } from "react";
-import {getTotalFromSheet} from "../utils/parse"
+import { getTotalFromSheet } from "../utils/parse";
 import { DataContext } from "../context/DataContext";
-
 
 const Stats = () => {
   const { data } = useContext(DataContext);
-  const cases = data.paCases.meta.total
-  const deaths = data.paDeaths.meta.total
+  const cases = data.paCases.meta.total;
+  const deaths = data.paDeaths.meta.total;
 
   return (
     <div className="stats__container">
-      <div className="stats__sub-container">
-        <div className="stats__title">Cases</div>
-        <div className="stats__number">{cases !== undefined ? cases : "..."}</div>
+      <div className="stats__row">
+        <div className="stats__col">
+          <span className="stats__title">Reported cases</span>
+        </div>
+        <div className="stats__col">
+          <span className="stats__title">Deaths</span>
+        </div>
       </div>
-      <div className="stats__number-container">
-        <div className="stats__title">Deaths</div>
-        <div  className="stats__number">{deaths !== undefined ? deaths : "..."}</div>
+      <div className="stats__row">
+        <div className="stats__col">
+          <span className="stats__number">
+            {cases !== undefined ? cases : "..."}
+          </span>
+        </div>
+        <div className="stats__col">
+          <span className="stats__number">
+            {deaths !== undefined ? deaths : "..."}
+          </span>
+        </div>
       </div>
     </div>
   );
