7326013841047c58d6d2e4d9c963de8d330f2c24
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-19 14:23:57 -0400

    Fix: Buttons form column on small screens

--- src/components/DataDisplayButtons.js
+++ src/components/DataDisplayButtons.js
@@ -4,7 +4,23 @@ const DataDisplayButtons = ({buttons, selected, handleClick}) => {
 
   return ( 
     <div className="data-display-buttons__container">
-      <div className="buttons are-normal has-addons is-centered">
+      <div className="data-display-buttons__container-mobile show-for-mobile">
+        {buttons.map((item, idx) => {
+          const selectedClass = item.id === selected ? 'is-primary' : ""
+          return (
+            <div key={idx} className="data-display-button__button-mobile">
+              <button 
+                className={`button is-normal is-fullwidth is-centered ${selectedClass}`}
+                key={item.id} 
+                onClick={handleClick}
+                id={item.id}>
+                {item.text}
+                </button>
+            </div>
+          )
+        })}
+      </div>
+      <div className="buttons are-normal has-addons is-centered show-for-desktop">
         {buttons.map(item => {
           const selectedClass = item.id === selected ? 'is-primary' : ""
           return (
