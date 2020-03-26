6403a3b7b6eedf232e4b039717ac14d11778d1b3
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-26 15:39:50 -0400

    Integrate props

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,18 +2,24 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faTimes } from "@fortawesome/free-solid-svg-icons";
 
-const Search = () => {
+const Search = ({inputText, clearResults, handleSearch}) => {
   return (
     <div className="search__container">
       <div className="field has-addons">
         <p className="control">
           <a className="button is-dark">Search</a>
         </p>
-        <p className="control">
-          <input className="input" type="text" placeholder="county" />
+        <p className="control" >
+          <input 
+          className="input" 
+          placeholder="county" 
+          type="text" 
+          onChange={handleSearch}
+          value={inputText}
+          />
         </p>
         <p className="control">
-          <a className="button is-light">
+          <a className="button is-light" onClick={clearResults}>
             <FontAwesomeIcon icon={faTimes}  />
           </a>
         </p>
