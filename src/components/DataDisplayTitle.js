414ca2d17b06ba7b8d2b7e4a0ee698519faa97ce
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-27 16:38:05 -0400

    Prettify

--- src/components/DataDisplayTitle.js
+++ src/components/DataDisplayTitle.js
@@ -1,12 +1,11 @@
-import React from 'react';
+import React from "react";
 
-const DataDisplayTitle = ({title}) => {
-  return ( 
+const DataDisplayTitle = ({ title }) => {
+  return (
     <div className="data-display-title__container">
-      <div className="data-display-title__title">
-        {title}
-      </div>
-    </div>  );
-}
- 
+      <div className="data-display-title__title">{title}</div>
+    </div>
+  );
+};
+
 export default DataDisplayTitle;
