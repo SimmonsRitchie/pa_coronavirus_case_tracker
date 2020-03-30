f0929714aa5767547b4484e32077b19e24154258
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-30 17:54:25 -0400

    Change component name

--- src/components/TestsChartCrosshair.js
+++ src/components/TestsChartCrosshair.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import { format } from 'd3-format'
-
 const CrosshairData = ({values}) => {
   const formatComma = format(",")
   const positiveTests = values[0].y
   const negativeTests = values[1].y
+  const totalTests = positiveTests + negativeTests
   const percentPos = positiveTests / ( positiveTests + negativeTests)
   const formatPercent = format(".1%")
 
   return (
-    <div className="tests-chart__crosshair-container">
-    <div className="tests-chart__crosshair-label">
+    <div className="tests-chart-crosshair__container">
+    <div className="tests-chart-crosshair__label">
       {values[0].x.format("MMM D")}
     </div>
-    <div>{formatComma(positiveTests)} positive</div>
-    <div>{formatComma(negativeTests)} negative</div>
-    <div>{formatPercent(percentPos)} positive</div>
+    <div>Positive: {formatPercent(percentPos)}</div>
+    <div>Total: {formatComma(totalTests)}</div>
   </div>
   )
 }
