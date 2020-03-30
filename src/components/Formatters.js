1e6e472c273ddb47082e4c5dc19eea6fa2bcf52b
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-30 15:53:44 -0400

    Add comments

--- src/components/Formatters.js
+++ src/components/Formatters.js
@@ -1,10 +1,13 @@
 import React from 'react';
 
 export const Dot = () => {
+  // Renders a little dot
   return ( <span className="formatters__separator"/> );
 }
 
 export const B = (props) => {
+  // Wraps text in custom bold formatter
   return ( <span className="formatters__bold">{props.children}</span> );
 }
  
+
