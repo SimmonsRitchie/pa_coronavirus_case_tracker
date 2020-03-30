0cd85ef9b8c416e57d8d71551e77e7d6013419eb
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-30 17:23:50 -0400

    Add conditional rendering for single, static button

--- src/components/DataDisplayToggles.js
+++ src/components/DataDisplayToggles.js
@@ -2,21 +2,31 @@ import React from 'react';
 
 const DataDisplayToggles = ({buttons, selected, handleButtonClick}) => {
 
-  return ( 
-    <div className="data-display-toggles__container">
-      <div className="buttons are-normal has-addons is-centered">
-        {buttons.map(item => {
-          const selectedClass = item.type === selected ? 'is-dark' : 'is-light'
-          return <button 
-          key={item.type} 
-          id={item.type} 
-          className={`button ${selectedClass}`} 
-          onClick={() => handleButtonClick(item.type)}>
-            {item.buttonText}
-          </button>
-        })}
+  if (buttons.length > 1) {
+    return ( 
+      <div className="data-display-toggles__container">
+        <div className="buttons are-normal has-addons is-centered">
+          {buttons.map(item => {
+            const selectedClass = item.type === selected ? 'is-dark' : 'is-light'
+            return <button 
+            key={item.type} 
+            id={item.type} 
+            className={`button ${selectedClass}`} 
+            onClick={() => handleButtonClick(item.type)}>
+              {item.buttonText}
+            </button>
+          })}
+        </div>
+      </div> );
+  } else {
+    return (
+      <div className="data-display-toggles__container">
+        <button className="button is-dark data-display-toggles__disabled">{buttons[0].buttonText}</button>
       </div>
-    </div> );
+
+    )
+  }
+
 }
  
 export default DataDisplayToggles;
