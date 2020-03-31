7350c2d3a5d777d86e9cb5654a4fedcf87efe48f
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-03-31 10:07:41 -0400

    Fix: adjust text, per Baxter recommendation

--- src/utils/textFormat.js
+++ src/utils/textFormat.js
@@ -5,18 +5,26 @@ export const genTestsDescrip = data => {
   const formatPercent = format(".1%");
   const formatComma = format(",");
   const tests = data.paTests;
-  const { mostRecentDate } = tests.meta;
+  console.log(tests);
+  const { mostRecentDate, oldestDate } = tests.meta;
   const formatMostRecentDate = mostRecentDate.format("MMM D");
-  const positiveTests = tests.testData.find(item => item.category === "positive").total
-  const totalTested = tests.testData.find(item => item.category === "total").total
-  const cleanPercPos = formatPercent(positiveTests / totalTested)
-  const cleanTotalTested = formatComma(totalTested)
+  const formatOldestDate = oldestDate.format("MMM D");
+  const positiveTests = tests.testData.find(
+    item => item.category === "positive"
+  ).total;
+  const totalTested = tests.testData.find(item => item.category === "total")
+    .total;
+  const cleanPercPos = formatPercent(positiveTests / totalTested);
+  const cleanTotalTested = formatComma(totalTested);
 
+  //Since March XX, a total of XXXXX Pennsylvanians have been tested for the coronavirus, and XXX were positive
   const descrip = (
     <span>
-      <B>{cleanPercPos}</B> of <B>{cleanTotalTested}</B> Pennsylvanians have
-      tested positive for the Coronavirus as of <B>{formatMostRecentDate}</B>. The chart shows a running total
-       of <LegText series={2}>positive</LegText> and <LegText series={1}>negative</LegText> results.
+      Since <B>{formatOldestDate}</B>, a total of <B>{cleanTotalTested}</B> Pennsylvanians{" "}
+      have been tested for the coronavirus, and <B>{cleanPercPos}</B> were{" "}
+      positive. The chart shows a running total of{" "}
+      <LegText series={2}>positive</LegText> and{" "}
+      <LegText series={1}>negative</LegText> results.
     </span>
   );
   return descrip;
