96a846e602939bc3dd9ce10ba03463c0f2f19068
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-04-06 09:44:07 -0400

    Remove obsolete import, format

--- src/components/DataTests/DataTests.js
+++ src/components/DataTests/DataTests.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { DataContext } from "../../context/DataContext"
+import { DataContext } from "../../context/DataContext";
 import DataDisplayContainer from "../DataDisplayContainer";
 import DataDisplayDesc from "../DataDisplayDesc";
 import DataDisplaySubContainer from "../DataDisplaySubContainer";
@@ -7,15 +7,13 @@ import DataDisplayVizContainer from "../DataDisplayVizContainer";
 import { createXYPoints } from "../../utils/parse";
 import TestsChart from "./Chart";
 import { genTestsDescrip } from "../../utils/textFormat";
-import { xTickCalc} from "../../utils/chartHelpers"
-import DataDisplayToggles from "../DataDisplayToggles";
-
+import { xTickCalc } from "../../utils/chartHelpers";
 
 class DataTests extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      xYPointsArr: []
+      xYPointsArr: [],
     };
   }
 
@@ -25,25 +23,27 @@ class DataTests extends React.Component {
     const { data } = this.context;
     // GET TESTS
     const tests = data.paTests.testData;
-    const posTests = tests.filter(item => item.category === "positive")[0];
-    const negTests = tests.filter(item => item.category === "negative")[0];
+    const posTests = tests.filter((item) => item.category === "positive")[0];
+    const negTests = tests.filter((item) => item.category === "negative")[0];
     const posPoints = createXYPoints(posTests);
     const negPoints = createXYPoints(negTests);
     const xYPointsArr = [posPoints, negPoints];
     this.setState({
-      xYPointsArr
+      xYPointsArr,
     });
   }
 
   render() {
     const { xYPointsArr } = this.state;
     const { data } = this.context;
-    const TESTS = [{
-      type: "tests",
-      yAxisTickTotal: 5,
-      buttonText: "Tests",
-      desc: genTestsDescrip(data)
-    }];
+    const TESTS = [
+      {
+        type: "tests",
+        yAxisTickTotal: 5,
+        buttonText: "Tests",
+        desc: genTestsDescrip(data),
+      },
+    ];
     const screenWidth = window.innerWidth;
     const xTickTotal = xTickCalc(screenWidth);
 
