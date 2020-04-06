589472776b58a69e5cb51543ba9b456017d4dc37
Daniel Simmons-Ritchie <daniel.simmons.ritchie@gmail.com>
2020-04-06 10:47:13 -0400

    Add d3 formatters

--- src/utils/chartHelpers.js
+++ src/utils/chartHelpers.js
@@ -1,6 +1,6 @@
+import { format } from "d3-format";
 
-
-export const xTickCalc = screenWidth => {
+export const xTickCalc = (screenWidth) => {
   if (screenWidth < 550) {
     return 3;
   } else {
@@ -8,3 +8,8 @@ export const xTickCalc = screenWidth => {
   }
 };
 
+// Formats thousands, 1000 becomes 1k, etc.
+export const formatK = format("~s");
+
+// Formats commas, 1000 becomes 1,000
+export const formatComma = format(",");
